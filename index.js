9de0929e6a4df6caf060bf1c22a45175cd8c13f1
Renan Mariano <renanbym@gmail.com>
2019-06-07 17:04:30 +0100

    ..

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #! /usr/bin/env node
 const cm = require('commander');
 const { HLTV } = require('hltv');
+const colors = require('colors');
+
+colors.setTheme({
+    custom: ['red', 'underline']
+});
 
 cm
     .version('1.0.0')
@@ -14,14 +19,15 @@ cm
 cm.parse(process.argv);
 
 async function xama(idmatch) {
-  
+
     HLTV.connectToScorebot({
         id: idmatch,
         onScoreboardUpdate: (data) => {
-            console.log(`(${data.CT.length}) ${data.ctTeamName} ${data.ctTeamScore} X ${data.tTeamScore} ${data.terroristTeamName} (${data.TERRORIST.length})`);
+            console.log(`(${colors.red(data.CT.filter(cur => cur.alive).length)}) ${data.ctTeamName} ${data.ctTeamScore} X ${data.tTeamScore} ${data.terroristTeamName} (${colors.red(data.TERRORIST.filter(cur => cur.alive).length)})`);
             console.log('')
+            console.log(`Round: ${data.currentRound}`);
             console.log(`Mapa: ${data.mapName}`);
-            console.log(`BombPlanted: ${data.bombPlanted}`);
+            console.log(`Bomb Planted: ${data.bombPlanted}`);
             process.exit();
         }
     });
